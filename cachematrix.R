129e2089b2f0efdf07decec3d0e413b8f5a3a181
rdulyarittir <rawin.dulyarittirong@gmail.com>
2016-10-30 22:16:20 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+# 
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inverse
+# get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get, setsolve = setsolve, getsolve=getsolve)
+        
 }
-
-
-## Write a short comment describing this function
+                
+# The following function calculates the inverse of the special "matrix" created with the above function.
+# However, it first checks to see if the inverse has already been calculated. 
+# If so, it gets the inverse from the cache and skips the computation. 
+# Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setsolve function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
+
+# #uncomment below to test the code
+# 
+# test_matrix <- matrix(
+#  c(3, 3.5, 3.2, 3.6), # the data elements
+#         nrow=2,              # number of rows
+#         ncol=2,              # number of columns
+#         byrow = TRUE)
+# 
+# test_matrix
+# 
+# a <- makeCacheMatrix(test_matrix)
+# 
+# cacheSolve(a)
+# 
+# #running it again will retreive from the cache
+# cacheSolve(a)
